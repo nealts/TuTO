cd577bf1175666ab9a361135d107fee8a3c63a28
saussol nicolas <saussol.nicolas@gmail.com>
2020-03-10 18:39:17 +0100

    firt

---
+++ juli1 tu pue la merde/juli1 tu pue la merde/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juli1_tu_pue_la_merde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
